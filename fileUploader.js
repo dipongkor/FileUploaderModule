8da25c49e5c68ad31760c907694a30d5f74e5ce8
dipongkor <atish@bs-23.com>
2015-02-23 11:22:55 +0600

    initial commit

---
+++ fileUploader.js
@@ -0,0 +1,53 @@
+﻿"use strict"
+var akFileUploaderModule;
+(function () {
+    akFileUploaderModule = angular.module("akFileUploader", []);
+
+    akFileUploaderModule.factory("akFileUploaderService", ["$q", "$http",
+               function ($q, $http) {
+                   var getModelAsFormData = function (data) {
+                       var dataAsFormData = new FormData();
+                       angular.forEach(data, function (value, key) {
+                           dataAsFormData.append(key, value);
+                       });
+                       return dataAsFormData;
+                   };
+
+                   var saveModel = function (data, url) {
+                       var deferred = $q.defer();
+                       $http({
+                           url: url,
+                           method: "POST",
+                           data: getModelAsFormData(data),
+                           transformRequest: angular.identity,
+                           headers: { 'Content-Type': undefined }
+                       }).success(function (result) {
+                           deferred.resolve(result);
+                       }).error(function (result, status) {
+                           deferred.reject(status);
+                       });
+                       return deferred.promise;
+                   };
+
+                   return {
+                       saveModel: saveModel
+                   }
+
+               }]);
+
+    akFileUploaderModule.directive("akFileModel", ["$parse",
+                function ($parse) {
+                    return {
+                        restrict: "A",
+                        link: function (scope, element, attrs) {
+                            var model = $parse(attrs.akFileModel);
+                            var modelSetter = model.assign;
+                            element.bind("change", function () {
+                                scope.$apply(function () {
+                                    modelSetter(scope, element[0].files[0]);
+                                });
+                            });
+                        }
+                    };
+                }]);
+})();
